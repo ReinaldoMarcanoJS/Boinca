d2f0431dbe23155e55c920dd1d4ace5fa91c5aa7
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2024-05-19 10:22:42 -0400

    hola

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className="flex flex-col h-[100vh] justify-center items-center w-full  bg-cyan-950/80 ">
      
       <div className="flex flex-col h-[100vh] justify-center items-center w-full    p-4 px-32 bg-transparent">
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia, praesentium quas eveniet veniam distinctio culpa nulla saepe? Laborum, cupiditate magnam praesentium debitis vel delectus quae rem? Dolor natus recusandae itaque?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. ia, praesentium quas eveniet veniam distinctio culpa nulla saepe? Laborum, cupiditate magnam praesentium debitis vel delectus quae rem? Dolor natus recusandae itaque?</p>
           </div>
           
             
