a08ec808de6f4f6f4d40ff20c66e36ad0d30a417
Reinaldo Marcano <reynaldomarcano4@gmail.com>
2024-09-13 10:43:09 -0400

    emailjs

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,37 +1,30 @@
 import { message } from "antd";
 import emailjs from "@emailjs/browser";
-import { Ref, RefObject } from "react";
+import { RefObject } from "react";
 
 type props = {
-    actions: any, 
-    form: RefObject<HTMLFormElement>
-}
+  actions: any;
+  form: RefObject<HTMLFormElement>;
+};
 
-export const formhandle = ({actions, form} : props) => {
+export const formhandle = ({ actions, form }: props) => {
   emailjs
-  .sendForm(
-    "service_paoz125",
-    "template_nxofspx",
-    form.current!,
-    {
-      publicKey: "Xkko0Kfz-kKcRYM4S",
-    }
-  )
-   .then(
-    message.open({
-      type: "success",
-      content: "Correo enviado correctamente",
+    .sendForm("service_j4tya15", "template_uwb82mo", form.current!, {
+      publicKey: "-thb_WDdZqhn6d1i3",
     })
+    .then(
+      message.open({
+        type: "success",
+        content: "Correo enviado correctamente",
+      })
     ),
-    (error : Error) => {
-        message.open({
-          type: "error",
-          content: "Oh! algo ha salido mal",
-        });
+    (error: Error) => {
+      message.open({
+        type: "error",
+        content: "Oh! algo ha salido mal",
+      });
       console.error("Error al enviar el formulario", error);
-      return
-    }
-    actions.resetForm();
-
-  
+      return;
+    };
+  actions.resetForm();
 };
